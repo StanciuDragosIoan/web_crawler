c369d5c3a644ec5f8eb51ead909f6cad69777cc4
stanciudragosioan <stanciudragosioan92@gmail.com>
2023-08-24 20:09:55 +0300

    add domain extraction too

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-
-const Xray = require('x-ray');
-const cheerio = require('cheerio');
-const x = Xray();
-
-const csv = require('csv-parser');
-
-
-
-
 const axios = require('axios');
 const fs = require('fs');
 
@@ -26,27 +16,30 @@ const urls = [
 ];
 
 function extractLinksAndContent(html) {
-    // Simple regex to match anchor tags
+    // get anchor tags
     const anchorTagRegex = /<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/g;
     const links = [];
     let match;
     while ((match = anchorTagRegex.exec(html)) !== null) {
         const link = match[2];
         links.push(link);
-        // You can process the link here
-
     }
 
-    // Extract phone numbers (simple pattern matching)
+    // Extract phone numbers
     const phoneRegex = /\b\d{3}-\d{3}-\d{4}\b/g;
     const phoneMatches = html.match(phoneRegex);
     console.log('Phone Numbers:', phoneMatches);
 
-    // Extract company names(simple pattern matching)
+    // Extract company names
     const companyRegex = /<title>(.*?)<\/title>/;
     const companyMatch = html.match(companyRegex);
     const companyName = companyMatch ? companyMatch[1] : '';
-    console.log('Company Name:', companyName);
+
+    // Extract domain
+    const domainRegex = /https?:\/\/(?:www\.)?([^/]+)\//i;
+    const domainMatch = html.match(domainRegex);
+    const domain = domainMatch ? domainMatch[1] : '';
+
 
     // Check if the extracted company name contains common unwanted patterns
     const unwantedPatterns = ['Customer Reviews', 'Contact Us', "Author at"];
@@ -55,28 +48,26 @@ function extractLinksAndContent(html) {
         companyName
     );
 
-    console.log('Company Name:', filteredCompanyNames);
-    // Extract social media links (simple pattern matching)
+
+    // Extract social media links  
     const socialMediaRegex = /href=["'](https?:\/\/(?:www\.)?(?:facebook|twitter|linkedin)\.[a-z\.]+)/g;
     const socialMediaMatches = html.match(socialMediaRegex);
-    console.log('Social Media Links:', socialMediaMatches);
+
 
     // Extract address/location information (simple pattern matching)
     const addressRegex = /(?:[A-Z][a-z]+,\s*){2,}[A-Z][a-z]+/g;
     const addressMatches = html.match(addressRegex);
-    console.log('Address/Location:', addressMatches);
-
 
     //write to file
-    const extractedData = {
-        domain: 'example.com', // Replace with actual domain
-        company_commercial_name: 'Commercial Name',
-        company_legal_name: 'Legal Name',
-        company_all_available_names: 'All Available Names',
-        social_links: 'Social Links', // Update with actual links
-        phone: 'Phone Number', // Update with actual phone number
-        address: 'Address' // Update with actual address
-    };
+    const newRow = [
+        `${domain}`, // Replace with actual domain
+        `${filteredCompanyNames}`,
+        `${filteredCompanyNames}`,
+        `${filteredCompanyNames}`,
+        `${socialMediaMatches}`, // Update with actual links
+        `${phoneMatches}`, // Update with actual phone number
+        `${addressMatches}` // Update with actual address
+    ];
 
     // Extract data using regular expressions and store in entries array
     const entries = [];
@@ -86,7 +77,7 @@ function extractLinksAndContent(html) {
 
     const rawContent = fs.readFileSync('testWrite.csv', 'utf-8');
     const lines = rawContent.trim().split('\n');
-    const newLines = [...lines, row.join(",")];
+    const newLines = [...lines, newRow.join(",")];
     const csvContent = newLines.join('\n');
 
     fs.writeFileSync("testWrite.csv", csvContent)
@@ -158,7 +149,7 @@ async function main() {
 
     for (const url of urls) {
         const data = await fetchData(url);
-        const { links } = extractLinksAndContent(data)
+        const { links } = extractLinksAndContent(data, url)
         if (data) {
             responseData.push({ url, links });
         }
